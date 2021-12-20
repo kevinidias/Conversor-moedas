ac563a0bfa00512569a9476eb02af702b35cfbaf
Kevin <kevin.dias.20160@gmail.com>
2021-12-20 20:15:11 -0300

    create interface

---
+++ src/components/Picker/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import RNPickerSelect from 'react-native-picker-select';
+
+export default function Picker(){
+
+    const placeholder = {
+        label: 'Selecione uma moeda',
+        value: null,
+        color: '#000'
+    }
+
+    return(
+        <RNPickerSelect 
+            placeholder={placeholder}
+            items={[
+                {key: '1', label: 'USD', value: 'USD'},
+                {key: '2', label: 'EUR', value: 'EUR'},
+            ]}
+            onValueChange={ (value) => console.log(value)}
+            style={{
+                inputIOS:{
+                    fontSize: 20,
+                    color: '#000'
+                },
+                inputAndroid:{
+                    fontSize: 20,
+                    color: '#000'
+                }
+            }}
+        />
+    );
+}
